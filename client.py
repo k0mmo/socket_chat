d09acfc70f208f835949d8453f4d56f2ffb42519
k0mmo <k0mmo@protonmail.com>
2019-12-07 09:38:50 -0500

    Adjusted start up display to show what IP address client connected to. Adjusted start up display to show what Port number client connected to. adjusted start up display to show current version of client build. Adjusted spaceing in display to make the start up display look better.

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import sys, socket, threading
 import tincanchat
 
+ver = "0.0.1"
+
 HOST =  sys.argv[-1] if len(sys.argv) > 1 else '127.0.0.1'
 PORT = tincanchat.PORT
 
@@ -23,7 +25,12 @@ def handle_input(sock):
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
-    print('Connected to {}:{}'.format(HOST, PORT))
+    print("     Mercury Chat Client")
+    print('==============================')
+    print('[+] Connected to: {}'.format(HOST))
+    print('[+] On port # {}'.format(PORT))
+    print("[+] Client version: " + ver)
+    print('==============================\n')
 
     # Create thread for handling user input and message sending
     thread = threading.Thread(target=handle_input, args=[sock], daemon=True)
